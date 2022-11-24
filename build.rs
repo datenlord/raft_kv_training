95024f7f5665846bad9f905f419dee63ad5471a9
Jiawei Zhao <Phoenxi500526@163.com>
2022-11-24 23:12:51 +0800

    modify eraftpb.proto file

--- build.rs
+++ build.rs
@@ -2,7 +2,64 @@ use std::process::Command;
 
 fn main() {
     let mut config = prost_build::Config::new();
+    config.type_attribute(
+        "Entry",
+        "#[allow(missing_docs, clippy::missing_docs_in_private_items)]",
+    );
+    config.type_attribute(
+        "Entry.data",
+        "#![allow(missing_docs, clippy::missing_docs_in_private_items)]",
+    );
+    config.type_attribute(
+        "Message",
+        "#[allow(missing_docs, clippy::missing_docs_in_private_items)]",
+    );
+    config.type_attribute(
+        "Message.data",
+        "#![allow(missing_docs,clippy::missing_docs_in_private_items)]",
+    );
     config.bytes(["."]);
+    for item in [
+        "HardState",
+        "MsgHup",
+        "MsgBeat",
+        "MsgAppendResponse",
+        "MsgRequestVote",
+        "MsgRequestVoteResponse",
+        "MsgHeartbeat",
+        "MsgHeartbeatResponse",
+        "MsgSnapshot",
+        "MsgTimeoutNow",
+        "MsgTransferLeader",
+    ] {
+        config.type_attribute(item, "#[non_exhaustive] #[derive(Copy, Eq)]");
+    }
+    for item in [
+        "ConfState",
+        "Entry",
+        "Entry.data",
+        "EntryNormal",
+        "MsgPropose",
+        "MsgAppend",
+        "Message.data",
+        "Message",
+    ] {
+        config.type_attribute(item, "#[non_exhaustive]");
+    }
+
+    for item in [
+        "ConfState",
+        "Entry",
+        "Entry.data",
+        "EntryNormal",
+        "MsgPropose",
+        "MsgAppend",
+        "Message.data",
+        "Message",
+    ] {
+        config.type_attribute(item, "#[derive(Eq)]");
+    }
+
     config
         .out_dir("src/pb")
         .compile_protos(&["proto/eraftpb.proto"], &["."])
