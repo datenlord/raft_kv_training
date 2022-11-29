9eacf2bdc2e8f8ca0b3e8d1ff546b7682e5fa066
Jiawei Zhao <Phoenxi500526@163.com>
2022-11-29 17:03:14 +0800

    extract config definition to config.rs

---
+++ src/consensus/config.rs
@@ -0,0 +1,69 @@
+use crate::{RaftError, INVALID_ID};
+
+/// config definition
+#[non_exhaustive]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Config {
+    /// id is the identity of the local raft. id cannot be 0.
+    pub id: u64,
+
+    /// `election_timeout` is the number of Node.Tick invocations that must pass between
+    /// elections. That is, if a follower does not receive any message from the
+    /// leader of current term before `election_timeout` has elapsed, it will become
+    /// candidate and start an election. `election_timeout` must be greater than
+    /// `heartbeat_timeout`. We suggest `election_timeout` = 10 * `heartbeat_timeout` to avoid
+    /// unnecessary leader switching.
+    pub election_tick: usize,
+
+    /// `heartbeat_timeout` is the number of Node.Tick invocations that must pass between
+    /// heartbeats. That is, a leader sends heartbeat messages to maintain its
+    /// leadership every `heartbeat_timeout` ticks.
+    pub heartbeat_tick: usize,
+
+    /// Applied is the last applied index. It should only be set when restarting
+    /// raft. raft will not return entries to the application smaller or equal to Applied.
+    /// If Applied is unset when restarting, raft might return previous applied entries.
+    /// This is a very application dependent configuration.
+    pub applied: u64,
+}
+
+impl Default for Config {
+    #[inline]
+    fn default() -> Self {
+        /// A constant represents the default heartbeat tick of raft config.
+        const HEARTBEAT_TICK: usize = 2;
+        /// A constant represents the default election tick of raft config.
+        const ELECTION_TICK: usize = 20;
+        Self {
+            id: 0,
+            election_tick: ELECTION_TICK,
+            heartbeat_tick: HEARTBEAT_TICK,
+            applied: 0,
+        }
+    }
+}
+
+impl Config {
+    /// check if the given config is valid or not
+    ///
+    /// # Errors
+    /// It'll return `RaftError::InvalidConfig` if the id is invalid, the `heartbeat_tick` is lesser than
+    /// zero or the `election_tick` is smaller than the `heartbeat_tick`
+    #[inline]
+    pub fn validate(&self) -> Result<(), RaftError> {
+        if self.id == INVALID_ID {
+            return Err(RaftError::InvalidConfig("Invalid config ID".to_owned()));
+        }
+        if self.heartbeat_tick == 0 {
+            return Err(RaftError::InvalidConfig(
+                "Heartbeat timeout must be greater than 0".to_owned(),
+            ));
+        }
+        if self.election_tick <= self.heartbeat_tick {
+            return Err(RaftError::InvalidConfig(
+                "Election timeout must be greater than heartbeat timeout".to_owned(),
+            ));
+        }
+        Ok(())
+    }
+}
--- src/consensus/errors.rs
+++ src/consensus/errors.rs
@@ -2,15 +2,9 @@ use thiserror::Error;
 
 /// Error Definition in raft
 #[non_exhaustive]
-#[derive(Debug, Error, Copy, Clone)]
+#[derive(Debug, Error, Clone)]
 pub enum RaftError {
-    /// Invalid Config id
-    #[error("Cannot use none as id")]
-    InvalidConfID,
-    /// Invalid heartbeat timeout
-    #[error("heartbeat timeout must be greater than 0")]
-    InvalidHeartbeatTimeout,
-    /// Invalid election timeout
-    #[error("election tick must be greater than heartbeat tick")]
-    InvalidElectionTimeout,
+    /// Invalid Config
+    #[error("{0}")]
+    InvalidConfig(String),
 }
