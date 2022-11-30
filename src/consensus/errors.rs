1077b7d66d7871aa6d1a234ad77999d6dbe728bf
Jiawei Zhao <Phoenxi500526@163.com>
2022-11-30 23:23:47 +0800

    implement storage struct

--- src/consensus/errors.rs
+++ src/consensus/errors.rs
@@ -2,9 +2,102 @@ use thiserror::Error;
 
 /// Error Definition in raft
 #[non_exhaustive]
-#[derive(Debug, Error, Clone)]
+#[derive(Debug, Error)]
 pub enum RaftError {
     /// Invalid Config
     #[error("{0}")]
     InvalidConfig(String),
+    /// Storage relevant errors
+    #[error("{0}")]
+    Store(#[from] StorageError),
+}
+
+impl PartialEq for RaftError {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (&RaftError::Store(ref e1), &RaftError::Store(ref e2)) => e1 == e2,
+            (&RaftError::InvalidConfig(ref e1), &RaftError::InvalidConfig(ref e2)) => e1 == e2,
+            _ => false,
+        }
+    }
+}
+
+/// An error with the storage
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum StorageError {
+    /// Log Unavailable
+    #[error("log unavailable: last index: {0}, target index: {1}")]
+    Unavailable(u64, u64),
+
+    /// Empty Entries
+    #[error("empty entries")]
+    EmptyEntries(),
+
+    /// Some other errors occurred
+    #[error("unknown error {0}")]
+    Others(#[from] Box<dyn std::error::Error + Send + Sync>),
+}
+
+impl PartialEq for StorageError {
+    #[allow(clippy::match_same_arms)]
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (&StorageError::Unavailable(i1, i2), &StorageError::Unavailable(i3, i4)) => {
+                i1 == i3 && i2 == i4
+            }
+            (&StorageError::EmptyEntries(), &StorageError::EmptyEntries()) => true,
+            _ => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_raft_error_equal() {
+        assert_eq!(
+            RaftError::InvalidConfig("Invalid config ID".to_owned()),
+            RaftError::InvalidConfig("Invalid config ID".to_owned())
+        );
+
+        assert_ne!(
+            RaftError::InvalidConfig("Invalid config ID".to_owned()),
+            RaftError::InvalidConfig("Heartbeat timeout must be greater than 0".to_owned())
+        );
+        assert_ne!(
+            RaftError::Store(StorageError::EmptyEntries()),
+            RaftError::Store(StorageError::Unavailable(1, 2))
+        );
+        assert_eq!(
+            RaftError::Store(StorageError::Unavailable(1, 2)),
+            RaftError::Store(StorageError::Unavailable(1, 2))
+        );
+    }
+
+    #[test]
+    fn test_storage_error_equal() {
+        assert_eq!(
+            StorageError::Unavailable(1, 2),
+            StorageError::Unavailable(1, 2)
+        );
+
+        assert_ne!(
+            StorageError::Unavailable(1, 2),
+            StorageError::Unavailable(3, 4)
+        );
+
+        assert_ne!(
+            StorageError::Unavailable(1, 2),
+            StorageError::EmptyEntries()
+        );
+        assert_ne!(
+            StorageError::Others(Box::new(StorageError::EmptyEntries())),
+            StorageError::Unavailable(1, 2)
+        );
+    }
 }
--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,2 +1,16 @@
+use crate::ConfState;
 /// raft message and relevant data structure definition
 pub(crate) mod eraftpb;
+
+impl<Iter> From<Iter> for ConfState
+where
+    Iter: IntoIterator<Item = u64>,
+{
+    #[must_use]
+    #[inline]
+    fn from(nodes: Iter) -> Self {
+        let mut conf_state = ConfState::default();
+        conf_state.nodes.extend(nodes.into_iter());
+        conf_state
+    }
+}
